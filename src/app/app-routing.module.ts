d2431faa53ff40e2403f823bc603e5b460093407
FrankzeB <franezequielbravo@outlook.com>
2023-06-29 19:02:32 -0300

    creacion de rama (lazyloading) auth routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //LazyLoading
+  {path: 'auth',
+  loadChildren:()=>import("./modules/auth/auth.module").then( m => m.AuthModule)} //creacion de lazyload (auth)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
